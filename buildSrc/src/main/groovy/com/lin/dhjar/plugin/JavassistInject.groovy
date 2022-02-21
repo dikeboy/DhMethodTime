883c6a709e741f224a5c8195b5ec7395f40302dc
dikeboy <874720637@qq.com>
2022-02-21 19:05:39 +0800

    Update JavassistInject.groovy

--- buildSrc/src/main/groovy/com/lin/dhjar/plugin/JavassistInject.groovy
+++ buildSrc/src/main/groovy/com/lin/dhjar/plugin/JavassistInject.groovy
@@ -33,7 +33,7 @@ class JavassistInject {
     }
 
 
-    static void injectDir(String inputPath, String outPutPath, ClassPool mClassPool, LJarConfig lJarConfig) {
+    static void injectDir(String inputPath, String outPutPath, ClassPool mClassPool,LJarConfig lJarConfig) {
         File dir = new File(inputPath)
         if (dir.isDirectory()) {
             dir.eachFileRecurse { File file ->
@@ -49,8 +49,8 @@ class JavassistInject {
                             && !filePath.contains("BuildConfig.class")) {
                         FileInputStream inputStream = new FileInputStream(file)
                         FileOutputStream outputStream = new FileOutputStream(outPutFile)
-                        System.out.println("out put ===========" + outPutFile.getAbsolutePath())
-                        transformFile(inputStream, outputStream, mClassPool, lJarConfig)
+                        System.out.println("out put ==========="+outPutFile.getAbsolutePath())
+                        transformFile(inputStream, outputStream, mClassPool,lJarConfig)
                     } else {
                         FileUtils.copyFile(file, outPutFile)
                     }
@@ -59,7 +59,7 @@ class JavassistInject {
         }
     }
 
-    static void injectJar(File inputFile, File outFile, ClassPool mClassPool, LJarConfig lJarConfig) throws IOException {
+    static void injectJar(File inputFile, File outFile, ClassPool mClassPool,LJarConfig lJarConfig) throws IOException {
         ArrayList entries = new ArrayList()
         Files.createParentDirs(outFile)
         FileInputStream fis = null
@@ -81,7 +81,7 @@ class JavassistInject {
                             && !fileName.contains('R$')
                             && !fileName.contains('R.class')
                             && !fileName.contains("BuildConfig.class"))
-                        transform(zis, zos, mClassPool, lJarConfig)
+                        transform(zis, zos, mClassPool,lJarConfig)
 
                     else {
                         ByteStreams.copy(zis, zos)
@@ -104,41 +104,40 @@ class JavassistInject {
     }
 
 
-    static void transformFile(InputStream input, OutputStream out, ClassPool mClassPool, LJarConfig lJarConfig) {
+    static void transformFile(InputStream input, OutputStream out, ClassPool mClassPool,LJarConfig lJarConfig) {
         try {
             CtClass c = mClassPool.makeClass(input)
-            transformClass(c, mClassPool, lJarConfig)
+            transformClass(c, mClassPool,lJarConfig)
             out.write(c.toBytecode())
             c.detach()
             c.freeze()
         } catch (Exception e) {
             e.printStackTrace()
             throw new RuntimeException(e.getMessage())
-        } finally {
-            if (input != null) {
+        }finally{
+            if(input!=null){
                 input.close()
             }
-            if (out != null) {
+            if(out!=null){
                 out.close()
             }
         }
     }
-
-    static void transform(InputStream input, OutputStream out, ClassPool mClassPool, LJarConfig lJarConfig) {
+    static void transform(InputStream input, OutputStream out, ClassPool mClassPool,LJarConfig lJarConfig) {
         try {
             CtClass c = mClassPool.makeClass(input)
-            transformClass(c, mClassPool, lJarConfig)
+            transformClass(c, mClassPool,lJarConfig)
             out.write(c.toBytecode())
             c.detach()
         } catch (Exception e) {
             e.printStackTrace()
-            input.close()
             out.close()
+            input.close()
             throw new RuntimeException(e.getMessage())
         }
     }
 
-    private static void transformClass(CtClass c, ClassPool mClassPool, LJarConfig lJarConfig) {
+    private static void transformClass(CtClass c, ClassPool mClassPool,LJarConfig lJarConfig) {
 //        Set<String> keys = lJarConfig.cutList.keySet()
 //        for(String s: keys){
 //            if(c.getName().startsWith(s)){
@@ -146,72 +145,89 @@ class JavassistInject {
 //                break;
 //            }
 //        }
-        logMethodTime(c, mClassPool, lJarConfig)
+        logMethodTime(c,mClassPool,lJarConfig)
     }
-
-    private static void logMethodTime(CtClass c, ClassPool mClassPool, LJarConfig lJarConfig) {
+    private static void  logMethodTime(CtClass c, ClassPool mClassPool,LJarConfig lJarConfig) {
         if (c.isFrozen()) {
             c.defrost()
         }
-        if (c.isInterface() || c.isEnum())
+        if(c.isInterface()||c.isEnum())
             return
-//        if (c.getName().contains("\$"))
-//            return
-        for (String s : lJarConfig.exclude) {
-            if (c.getName().contains(s))
-                return
+        if(c.isEnum()){
+            return;
         }
+//        if(c.getName().contains("\$"))
+//            return
 
+        if(lJarConfig.include.size()>0){
+            boolean  has = false;
+            for(String s : lJarConfig.include){
+                if(c.getName().contains(s)){
+                    has=true;
+                    break;
+                }
+            }
+            if(!has){
+                return;
+            }
+        }
+        if(lJarConfig.exclude.size()>0){
+            for(String s : lJarConfig.exclude){
+                if(c.getName().contains(s))
+                    return
+            }
+        }
 
         int l = c.getDeclaredMethods().length
         Set<String> currentMethod = new HashSet<String>()
-        for (int i = 0; i < l; i++) {
-            CtMethod m = c.getDeclaredMethods()[i]
-            if (m.isEmpty())
-                continue
-//            if (m.getName().contains("\$"))
-//                continue
-//                if(m.getModifiers()== 25||m.getModifiers()== 9||m.getModifiers() ==8||m.getModifiers()==24)
+        for(int i=0;i<l;i++){
+            CtMethod  m = c.getDeclaredMethods()[i]
+                if(m.isEmpty())
+                    continue
+                if(m.getName().contains("\$"))
+                    continue
+//                if(m.getModifiers()== 25||m.getModifiers()==24)
 //                    continue
 
-            String param = "m" + m.getName() + "_" + i + "_" + System.nanoTime() % 1000
-            if (currentMethod.contains(param)) {
-                param = param + "a1"
-            }
-            currentMethod.add(param)
-            CtField ctField = new CtField(CtClass.longType, param, c)
-            ctField.setModifiers(9)
-            c.addField(ctField, "0l")
-
-
-            String localName = param + "_local"
-            m.addLocalVariable(localName, CtClass.longType)
-            try {
-                m.insertBefore(param + " =  System.currentTimeMillis();")
-                String userTime = " System.currentTimeMillis() - " + param + ""
-                String logName = lJarConfig.logEngine+"(\"" + lJarConfig.logFilter
-                String line = logName + "\",\"" + c.getName() + "::::" + m.getMethodInfo().getName() + "==\"+(" + userTime + "));"
-                int lineNum = 0;
-                if (m.getReturnType().getName().contains("void")) {
-                    lineNum = m.getMethodInfo().getLineNumber(m.getMethodInfo().codeAttribute.length())
-                } else {
-                    lineNum = m.getMethodInfo().getLineNumber(m.getMethodInfo().codeAttribute.length()) - 1
+                String param = "m"+m.getName()+"_"+i+"_"+System.nanoTime()%1000
+                if(currentMethod.contains(param)){
+                    param = param+"a1"
                 }
-                if (lineNum >= 0) {
-                    String resultTime = " if (" + userTime + " >=" + lJarConfig.logMinTime + ")" + line
-//                        System.out.println(resultTime)
-                    m.insertAfter(resultTime)
-                }
+                currentMethod.add(param)
+                CtField ctField = new CtField(CtClass.longType,param,c)
+                ctField.setModifiers(9)
+                c.addField(ctField,"0l")
 
-            } catch (CannotCompileException ex) {
+
+               String localName = param+"_local"
+                m.addLocalVariable(localName,CtClass.longType)
+            try{
+                m.insertBefore(param+" =  System.currentTimeMillis();")
+                String userTime =" System.currentTimeMillis() - "+param +""
+                String log = lJarConfig.logType==null?LJarConfig.LOG_TYPE_LOG:lJarConfig.logType
+                String line = log+"(\""+lJarConfig.logFilter+"\",\""+c.getName()+"::::"+m.getMethodInfo().getName()+"====\"+("+userTime+"));"
+//                String line = "  System.out.println(\""+lJarConfig.logFilter+c.getName()+"::::"+m.getMethodInfo().getName()+"====\"+("+userTime+"));"
+                    int lineNum = 0;
+                    if(m.getReturnType().getName().contains("void")) {
+                         lineNum =  m.getMethodInfo().getLineNumber(m.getMethodInfo().codeAttribute.length())
+                    }else {
+                         lineNum =  m.getMethodInfo().getLineNumber(m.getMethodInfo().codeAttribute.length()) -1
+                    }
+                    if(lineNum>=0){
+                        String resultTime = " if (" + userTime + " >=" + lJarConfig.logMinTime + ")" + line
+                        m.insertAfter(resultTime)
+                    }
+
+                }catch(CannotCompileException ex){
 //                    ex.printStackTrace()
-                System.out.println(param + " get Error")
-            }
+                    System.out.println(param+" get Error")
+                }
 
 //                m.insertAfter( "System.out.println("+param+");")
 //                m.insertAfter()
         }
 
     }
+
 
 }
