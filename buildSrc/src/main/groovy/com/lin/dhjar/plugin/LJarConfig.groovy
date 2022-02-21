06fecbe1c5ded822c5f9a36fe503ea609928cc92
dikeboy <874720637@qq.com>
2022-02-21 19:06:00 +0800

    Update LJarConfig.groovy

--- buildSrc/src/main/groovy/com/lin/dhjar/plugin/LJarConfig.groovy
+++ buildSrc/src/main/groovy/com/lin/dhjar/plugin/LJarConfig.groovy
@@ -3,17 +3,29 @@ package com.lin.dhjar.plugin
 
 class LJarConfig {
     List<String> exclude = new  ArrayList<String>() //exclude class like
+    List<String> include =new ArrayList<String>();
     long  logMinTime = 0l
-    String logFilter = "[dhmethodtime] "
-    String logEngine = "android.util.Log.i"
+    String logFilter = "[tencentCloudGame] "
+    public static String LOG_TYPE_LOG ="android.util.Log.i"
+    String pluginState = "debug"
+    String logType =LOG_TYPE_LOG
+
+
     /**
      * 需要 拦截的jar文件
      * @param filters
      * @return
      */
-    LJarConfig getExclude(String...value) {
+    LJarConfig setExclude(String...value) {
         if (value != null) {
             exclude.addAll(value)
+        }
+        return this
+    }
+
+    LJarConfig setInclude(String...value) {
+        if (value != null) {
+            include.addAll(value)
         }
         return this
     }
@@ -25,16 +37,23 @@ class LJarConfig {
         return this
     }
 
-    LJarConfig setFilter(String filter) {
-        if (filter != null) {
-            logFilter = filter
+    LJarConfig setLogFilter(String mLogFilter){
+        if(mLogFilter!=null&&mLogFilter.size()>0){
+            logFilter = mLogFilter
         }
         return this
     }
 
-    LJarConfig setLogEngine(String engine) {
-        if (engine != null&&engine.length()>0) {
-            logEngine = engine
+    LJarConfig setLogType(String mLogType){
+        if(mLogType!=null&&mLogType.size()>1){
+            logType = mLogType
+        }
+        return this
+    }
+
+    LJarConfig setPluginState(String state){
+        if(state!=null){
+            pluginState = state
         }
         return this
     }
